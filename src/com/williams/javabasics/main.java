7104adb53a39cf5008ae2befef0fc71619ad86d0
Williams David <williamsdavidgaitanolaya@gmail.com>
2025-12-21 17:12:20 -0500

    Day 2:Read user input with Scanner and validate age

--- src/com/williams/javabasics/main.java
+++ src/com/williams/javabasics/main.java
@@ -1,19 +1,42 @@
+/*
+ * Day 2 - Java Basics
+ *
+ * This program reads the user's age from the console using Scanner
+ * and determines whether the user is an adult or a minor.
+ *
+ * Concepts applied:
+ * - User input with Scanner
+ * - Conditional statements (if / else)
+ * - Console output
+ */
+
+
+
 package com.williams.javabasics;
 
-public class main {
+import java.util.Scanner;
+
+public class Main {
 	
-	public static void Main(String[] args) {
+	public static void main(String[] args) {
 		
-		//simple program to check if a person is an adult
+		 
+		Scanner scanner = new Scanner(System.in);
 		
-		int age = 17;
+		System.out.println("enter you age :");
+		int age =scanner.nextInt();
+		
+		
 		
 		if (age>=18 ){
-		System.out.println("adult");	
+		System.out.println("You are an adult");	
 		}
 		
 		else{
-	    System.out.println("Minor");
+	    System.out.println("You are a minor");
+	    
+	    scanner.close();
+	    
 		}
 	
 	}
