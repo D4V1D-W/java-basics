4fc4088ee7d402a0594c57e7462105ebafe381a2
Williams David <williamsdavidgaitanolaya@gmail.com>
2025-12-21 12:08:28 -0500

    firts commit - java basics

---
+++ src/com/williams/javabasics/main.java
@@ -0,0 +1,22 @@
+package com.williams.javabasics;
+
+public class main {
+	
+	public static void Main(String[] args) {
+		
+		//simple program to check if a person is an adult
+		
+		int age = 17;
+		
+		if (age>=18 ){
+		System.out.println("adult");	
+		}
+		
+		else{
+	    System.out.println("Minor");
+		}
+	
+	}
+
+
+}
